5115e8a79163b2627ee00d64f906e23fe5775645
diableJdy <okonghpp@naver.com>
2019-02-20 07:00:00 +0900

    기본 라우터

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,37 +1,14 @@
 import React, {Component} from 'react';
-import logo from './logo.svg';
-//mport './App.css';
-import TestComponents from "./components/TestComponents";
 import { hot } from 'react-hot-loader'
 
 class App extends Component {
-    state = {};
-
-    componentDidMount() {
-        //setInterval(this.hello, 250);
-    }
-
-    hello = () => {
-        fetch('/api/hello')
-            .then(response => response.text())
-            .then(message => {
-                this.setState({message: message});
-            });
-    };
 
     render() {
         return (
-            <div className="App">
-                <TestComponents/>
-                <div>
-                    {this.state.message}
-                </div>
-                <p className="App-intro">
-                    To get started, edit <code>src/App.js</code> and save to reload.111122333333
-                </p>
-            </div>
-        );
+            <h1>Home</h1>
+        )
     }
+
 }
 
 export default hot(module) (App);
--- frontend/src/App.test.js
+++ frontend/src/App.test.js
@@ -7,3 +7,33 @@ it('renders without crashing', () => {
   ReactDOM.render(<App />, div);
   ReactDOM.unmountComponentAtNode(div);
 });
+
+
+/*
+state = {};
+
+componentDidMount() {
+  //setInterval(this.hello, 250);
+}
+
+hello = () => {
+  fetch('/api/hello')
+      .then(response => response.text())
+      .then(message => {
+        this.setState({message: message});
+      });
+};
+
+render() {
+  return (
+      <div className="App">
+        <TestComponents/>
+        <div>
+          {this.state.message}
+        </div>
+        <p className="App-intro">
+          To get started, edit <code>src/App.js</code> and save to reload.111122333333
+        </p>
+      </div>
+  );
+}*/
