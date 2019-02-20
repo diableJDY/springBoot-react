741f652c35c3c15bd4cc72c032994db1f1538cc0
bgkim <intulda@naver.com>
2019-02-20 23:11:46 +0900

    검색으로 데이터 가져오기..

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,14 +1,62 @@
 import React, {Component} from 'react';
 import { hot } from 'react-hot-loader'
+import member from './components/Data';
 
 class App extends Component {
 
-    render() {
-        return (
-            <h1>Home</h1>
-        )
+    state = {
+        keyword : '',
+        name    : '',
+        age     : ''
     }
 
+    box = null;
+
+    setKeyword = (e) => {
+        this.setState({
+            keyword: e.target.value
+        });
+    }
+    setValue = (obj) => {
+        this.setState({
+            name : obj.name,
+            age  : obj.age
+        });
+    }
+    search = (e) => {
+        if(this.state.keyword != '') {
+            for(let value of member) {
+                if(value.name === this.state.keyword) {
+                    this.setValue(value);
+                    e.preventDefault();
+                } else {
+                    e.preventDefault();
+                }
+            }
+        } else {
+            e.preventDefault();
+        }
+    }
+
+    render() {
+        return (
+            <form onSubmit={this.search}>
+                <h1>Home</h1>
+                <hr/>
+
+                <input type={"text"} onChange={this.setKeyword} value={this.state.keyword}/>
+                <button type={onsubmit}>
+                    검색
+                </button>
+                <div ref={ref => this.box = ref}>
+                    <ul>
+                        <li>이름 : {this.state.name}</li>
+                        <li>나이 : {this.state.age}</li>
+                    </ul>
+                </div>
+            </form>
+        )
+    }
 }
 
 export default hot(module) (App);
---
+++ frontend/src/components/Data.js
@@ -0,0 +1,19 @@
+import React from 'react'
+
+
+let member = [
+    {
+        name : '김보근',
+        age  : 28
+    },
+    {
+        name : '문동현',
+        age  : 30
+    },
+    {
+        name : '박형길',
+        age  : 28
+    }
+    ]
+
+export default member;
