bd5629ddf8141a0f9828725a837e3571befd25f2
diableJdy <okonghpp@naver.com>
2019-06-29 06:38:15 +0900

    스웨거적용

--- src/main/java/com/start/react/Controller/TestController.java
+++ src/main/java/com/start/react/Controller/TestController.java
@@ -1,17 +1,33 @@
 package com.start.react.Controller;
 
 
+import com.start.react.mapper.TempMapper;
+import com.start.react.model.TempModel;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.Date;
+import java.util.List;
 
 @RestController
 public class TestController {
 
+    @Autowired
+    private TempMapper tempMapper;
+
     @GetMapping("/api/hello")
     public String hello() {
         return "Hello, the time at the server is now " + new Date() + "\n";
+    }
+
+    @RequestMapping(value = "/test/java")
+    public List<TempModel> whyJava(){
+
+        List<TempModel> tempLists = tempMapper.getTempList();
+        return tempLists;
+
     }
 
 }
